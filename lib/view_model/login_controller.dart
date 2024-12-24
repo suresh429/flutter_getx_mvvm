7aca875875e365910fb3248cea213dae281763f6
ThulasiramTAL <thulasiram.b@touchalife.org>
2024-12-24 16:40:02 +0530

    comment added

--- lib/view_model/login_controller.dart
+++ lib/view_model/login_controller.dart
@@ -7,7 +7,7 @@ import '../payload/login_payload.dart';
 import '../service/api_service.dart';
 import '../utilites/constants_Utils.dart';
 
-
+//login screen
 class LoginController extends GetxController {
   final storage = GetStorage();
 
